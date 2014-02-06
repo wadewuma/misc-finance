88750faf8e87dcb61513f0427a131b466b382f35
mkfs <semper.non.sequitur@gmail.com>
2014-02-06 01:07:47 -0500

    Added function comments. added functions to get company profile, symbol news. Improved symbol quote function.

--- R/fin.viz.R
+++ R/fin.viz.R
@@ -1,19 +1,26 @@
 #!/usr/bin/env Rscript
 # (c) Copyright 2014 mkfs <https://github.com/mkfs>
+# FinViz interface
 
 library(RCurl)
 library(XML)
 
+# FinViz URLs
 url.finviz.base <- 'http://finviz.com/export.ashx?o=ticker'
-#filter options:= &f=idx_sp500,sec_basicmaterials
+url.finviz.search <- 'http://finviz.com/search.ashx?'
 url.finviz.screener <- 'http://finviz.com/screener.ashx?ft=4'
+url.finviz.symbol <- 'http://finviz.com/quote.ashx?'
 
 # Naughty FinViz, checking UserAgent like that!
 finviz.user.agent <- 'Mozilla/5.0'
 
 
+# ------------------------------------------------------------------------
+# FILTERED QUERIES
 
-finviz.screener.options <- function() {
+# Helper function to return the parsed HTML of the FinViz Screener
+# "All Filters" page.
+finviz.screener.filters <- function() {
   html.raw <- getURLContent(url.finviz.screener, followlocation = TRUE, 
                             timeout = 100, useragent = finviz.user.agent)
   
@@ -25,7 +32,9 @@ finviz.screener.options <- function() {
   return( htmlParse(html.raw) )
 }
 
-build.name.value.dataframe <- function(data.names, data.values, prefix) {
+# Helper function to build a Name|Symbol dataframe from two character vectors
+# and a filter symbol prefix (e.g. ind, sec, idx, etc).
+build.name.symbol.dataframe <- function(data.names, data.values, prefix) {
   if (length(data.names) != length(data.values)) {
     warning(paste('Names has length', length(data.names),
                   'but Values has length', length(data.values)))
@@ -49,10 +58,12 @@ build.name.value.dataframe <- function(data.names, data.values, prefix) {
   return( df[nchar(df$Name) > 0,] )
 }
 
-finviz.extract.select.options <- function(id, prefix, html=NULL) {
+# Helper function to return a dataframe with the Name & Symbol of 
+# the specified filter options.
+finviz.extract.filter.options <- function(id, prefix, html=NULL) {
   
   if (is.null(html)) {
-    html <- finviz.screener.options()
+    html <- finviz.screener.filters()
     if (is.null(html)) {
       return(c())
     }
@@ -63,33 +74,42 @@ finviz.extract.select.options <- function(id, prefix, html=NULL) {
   path <- paste("//select[@id='", id, "']/option/@value", sep="")
   option.values <- xpathSApply(html, path)
   
-  return(build.name.value.dataframe(option.names, option.values, prefix))
+  return(build.name.symbol.dataframe(option.names, option.values, prefix))
 }
 
+# Return a dataframe with the Name & Symbol of all Sector filter options.
 finviz.sectors <- function(html=NULL) {
-  finviz.extract.select.options('fs_sec', 'sec', html)
+  finviz.extract.filter.options('fs_sec', 'sec', html)
 }
 
+# Return a dataframe with the Name & Symbol of all Industry filter options.
 finviz.industries <- function(html=NULL) {
-  finviz.extract.select.options('fs_ind', 'ind', html)
+  finviz.extract.filter.options('fs_ind', 'ind', html)
 }
 
+# Return a dataframe with the Name & Symbol of all Index filter options.
 finviz.indexes <- function(html=NULL) {
-  finviz.extract.select.options('fs_idx', 'idx', html)
+  finviz.extract.filter.options('fs_idx', 'idx', html)
 }
 
+# Return a dataframe with the Name & Symbol of all Exchange filter options.
 finviz.exchanges <- function(html=NULL) {
-  finviz.extract.select.options('fs_exch', 'exch', html)
+  finviz.extract.filter.options('fs_exch', 'exch', html)
 }
 
+# Return a dataframe with the Name & Symbol of all Location filter options.
 finviz.locations <- function(html=NULL) {
-  finviz.extract.select.options('fs_geo', 'geo', html)
+  finviz.extract.filter.options('fs_geo', 'geo', html)
 }
 
+# Return a dataframe with the Name & Symbol of all Market Cap filter options.
 finviz.market.caps <- function(html=NULL) {
-  finviz.extract.select.options('fs_cap', 'cap', html)
+  finviz.extract.filter.options('fs_cap', 'cap', html)
 }
 
+# Helper function to perform a query using the FinViz export URL.
+# The filters argument is a character vector containing one filter
+# symbol (e.g. "sec_financial") per element.
 finviz.query <- function(filters) {
   url <- paste(url.finviz.base, '&f=', paste(filters, collapse=','), sep='')
   csv <- getURLContent(url, followlocation = TRUE, timeout = 100, 
@@ -101,20 +121,117 @@ finviz.query <- function(filters) {
   }
 }
 
-finviz.sector.symbols <- function(industry.sym) {
+# Return a dataframe of all symbols in the specified sector.
+finviz.sector.symbols <- function(sector.sym) {
   finviz.query(sector.sym)
 }
 
+# Return a dataframe of all symbols in the specified industry.
 finviz.industry.symbols <- function(industry.sym) {
   finviz.query(industry.sym)
 }
 
-finviz.get.symbol <- function(sym) {
-  url <- paste(url.finviz.base, '&t=', paste(sym, collapse=','), sep='')
+# ------------------------------------------------------------------------
+# SYMBOL INFO
+
+# Helper function to return a specific page/view (112, 122, ...) from
+# the FinVis quote page for a symbol.
+finviz.get.symbol.page <- function(sym, page) {
+  url <- paste(url.finviz.base, '&v=', page, '&t=', 
+               paste(sym, collapse=','), sep='')
   csv <- getURLContent(url)
   if ((! is.na(csv)) && length(csv) > 0 ) {
     read.csv(textConnection(csv), header=TRUE)
   } else {
     c()
   }
+}
+
+# Return a dataframe containing the current quote for the specified symbol.
+finviz.get.symbol <- function(sym) {
+  sym.df <- NULL
+  for (i in c(112, 122, 132, 142, 162, 172)) {
+    df <- finviz.get.symbol.page(sym, i)
+    if (is.null(sym.df)) {
+      sym.df <- df
+    } else {
+      sym.df <- merge(sym.df, df)
+    }
+  }
+  return(sym.df)
+}
+
+# Returns a dataframe giving the latest news for the specified symbol.
+# Columns include Timestamp, URL, and Headline.
+finviz.symbol.news <- function(sym) {
+  url <- paste(url.finviz.symbol, "t=", sym, sep='')
+  html.raw <- getURLContent(url, followlocation = TRUE, timeout = 100, 
+                            useragent = finviz.user.agent)
+  if (is.na(html.raw) || length(html.raw) == 0) {
+    warning(paste("Read zero-length page from", url.finviz.search))
+    return(NULL)
+  }
+  
+  html <- htmlParse(html.raw)
+  
+  news.urls <- xpathSApply(html, 
+                           "//table[@class='fullview-news-outer']/tr/td/a/@href")
+  news.text <- xpathSApply(html, 
+                           "//table[@class='fullview-news-outer']/tr/td/a", 
+                           xmlValue)
+  news.ts <- xpathSApply(html, 
+                         "//table[@class='fullview-news-outer']/tr/td[@align='right']", 
+                         xmlValue)
+  # FIXME: Timestamp should be made a real datetime object, but the date 
+  #        component is not always present.
+  data.frame(Timestamp=news.ts, URL=news.urls, Headline=news.text)
+}
+
+# ------------------------------------------------------------------------
+# COMPANY INFO
+
+# Returns a dataframe containing company profile information from the
+# FinViz "company profile" search page:
+#   http://finviz.com/search.ashx?t=p&p=$QUERY
+# Note that this will search for a company name NOT ticker symbol.
+# Multiple rows will be returned if more than one company matches.
+finviz.company.profile <- function(query) {
+  url <- paste(url.finviz.search, "t=p&p=", query, sep='')
+  html.raw <- getURLContent(url, followlocation = TRUE, timeout = 100, 
+                            useragent = finviz.user.agent)
+  if (is.na(html.raw) || length(html.raw) == 0) {
+    warning(paste("Read zero-length page from", url.finviz.search))
+    return(NULL)
+  }
+  
+  html <- htmlParse(html.raw)
+  
+  rows <- xpathSApply(html, "//table/tr/td/table/tr/td", xmlValue)
+  num.results = length(rows) / 10
+  if (num.results < 1) {
+    warning(paste("No results found for '", query, "'", sep=''))
+    return(NULL)
+  }
+  
+  df <- data.frame( Ticker=character(num.results),
+                    Name=character(num.results),
+                    Sector=character(num.results),
+                    Industry=character(num.results),
+                    Location=character(num.results),
+                    MarketCap=character(num.results),
+                    Profile=character(num.results),
+                    stringsAsFactors=FALSE)
+  
+  for (i in 1:num.results) {
+    idx = (i - 1) * 10
+    df[i, 'Ticker'] = rows[idx + 2]
+    df[i, 'Name'] = rows[idx + 3]
+    df[i, 'Sector'] = rows[idx + 4]
+    df[i, 'Industry'] = rows[idx + 5]
+    df[i, 'Location'] = rows[idx + 6]
+    df[i, 'MarketCap'] = rows[idx + 7]
+    df[i, 'Profile'] = rows[idx + 9]
+  }
+
+  return(df)
 }
