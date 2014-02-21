0cf3cd1ccfbd9f7dd4d9fed0e34d0f4acccdaa2f
mkfs <semper.non.sequitur@gmail.com>
2014-02-21 01:29:43 -0500

    Added yahoo.symbol.quote. needs work.

--- R/yahoo.finance.R
+++ R/yahoo.finance.R
@@ -10,6 +10,8 @@ url.yahoo.finance.base <- 'http://biz.yahoo.com/p/csv'
 url.yahoo.finance.sector <- paste(url.yahoo.finance.base, 's_conameu.csv', 
                                   sep='/')
 url.yahoo.industry.list <- 'http://biz.yahoo.com/ic/ind_index_alpha.htm'
+url.yahoo.quote.base <- 'http://download.finance.yahoo.com/d/quotes.csv?s='
+#http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,GOOG&f=nsl1op
 
 # Helper function to safely download data using Curl.
 yahoo.download.csv.as.df <- function(url, set.id=TRUE) {
@@ -136,4 +138,52 @@ yahoo.symbol.details <- function( sym ) {
   vec <- vals[1:4]
   names(vec) <- keys[1:4]
   return(vec)
+}
+
+# Fields that can be supplied to Yahoo Quotes
+# FIXME: some of these screw up CSV parsing
+yahoo.symbol.quote.fields <- c( AfterHoursChangeRealtime='c8',
+      ##AnnualizedGain='g3', Ask='a', AskRealtime='b2', AskSize='a5',
+      ##AverageDailyVolume='a2', Bid='b', BidRealtime='b3', BidSize='b6',
+      #BookValuePerShare='b4', Change='c1', Change_ChangeInPercent='c',
+      ##ChangeFromFiftydayMovingAverage='m7',
+      #ChangeFromTwoHundreddayMovingAverage='m5', ChangeFromYearHigh='k4',
+      #ChangeFromYearLow='j5', ChangeInPercent='p2', 
+      #ChangeInPercentRealtime='k2', ChangeRealtime='c6', Commission='c3',
+      Currency='c4', DaysHigh='h', DaysLow='g', DaysRange='m',
+      #DaysRangeRealtime='m2', DaysValueChange='w1',
+      #DaysValueChangeRealtime='w4', DividendPayDate='r1',
+      ##TrailingAnnualDividendYield='d', 
+      ##TrailingAnnualDividendYieldInPercent='y', DilutedEPS='e',
+      ##EBITDA='j4', EPSEstimateCurrentYear='e7', EPSEstimateNextQuarter='e9',
+      ##EPSEstimateNextYear='e8', ExDividendDate='q', 
+      ##FiftydayMovingAverage='m3', SharesFloat='f6', HighLimit='l2',
+      HoldingsGain='g4', HoldingsGainPercent='g1',
+      HoldingsGainPercentRealtime='g5', HoldingsGainRealtime='g6',
+      HoldingsValue='v1', HoldingsValueRealtime='v7', LastTradeDate='d1',
+      LastTradePriceOnly='l1', LastTradeRealtimeWithTime='k1',
+      LastTradeSize='k3', LastTradeTime='t1', LastTradeWithTime='l',
+      LowLimit='l3', MarketCapitalization='j1', MarketCapRealtime='j3',
+      MoreInfo='i', Name='n', Notes='n4', OneyrTargetPrice='t8',
+      Open='o', OrderBookRealtime='i5', PEGRatio='r5', PERatio='r',
+      PERatioRealtime='r2', PercentChangeFromFiftydayMovingAverage='m8',
+      PercentChangeFromTwoHundreddayMovingAverage='m6',
+      ChangeInPercentFromYearHigh='k5', PercentChangeFromYearLow='j6',
+      PreviousClose='p', PriceBook='p6', PriceEPSEstimateCurrentYear='r6',
+      PriceEPSEstimateNextYear='r7', PricePaid='p1', PriceSales='p5',
+      ##Revenue='s6', SharesOwned='s1', SharesOutstanding='j2',
+      ShortRatio='s7', StockExchange='x', Symbol='s', TradeDate='d2',
+      # These are ASCII art and HTML <A> tags:
+      #TickerTrend='t7', TradeLinks='t6', TradeLinksAdditional='f',
+      TwoHundreddayMovingAverage='m4', Volume='v', YearHigh='k',
+      YearLow='j', YearRange='w' )
+
+yahoo.symbol.quote <- function(sym) {
+  fields <- paste(yahoo.symbol.quote.fields, collapse='')
+  syms <- paste(sym, collapse=',')
+  url <- paste(url.yahoo.quote.base, syms, '&f=', fields, '&e=.csv', sep='')
+  df.quote <- read.csv(url, header=FALSE, stringsAsFactors=FALSE,
+                       col.names=names(yahoo.symbol.quote.fields))
+  rownames(df.quote) <- df.quote$Symbol
+  return(df.quote)
 }
